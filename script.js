568cd07218a12bba9baac41de10581196b149d60
Vakho Lezhava <xxunxul@gmail.com>
2024-12-27 01:36:03 +0100

    afsfawfas

---
+++ script.js
@@ -0,0 +1,14 @@
+
+const buttons = document.querySelectorAll('.scroll-btn');
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        const target = button.getAttribute('data-target');
+        const targetSection = document.getElementById(`section-${target}`);
+        
+        targetSection.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+});
